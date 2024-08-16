7f0f13c3ae45c674671157bc64f5a3098d1871c0
Pavel Tatarintsev <ic472pbt@ya.ru>
2024-08-16 16:02:39 +0400

    allow duty to increase in float mode

--- MPPT/Charging.ino
+++ MPPT/Charging.ino
@@ -189,6 +189,7 @@ void Charging_Algorithm(float sol_volts, unsigned long currentTime) {
         }
         break;
       case bat_float:  
+        int effectiveBound = floatVoltageRaw + tempCompensationRaw - powerCompensation;
         mpptReached = 0;
         if(rawBatteryV > floatVoltageRaw + tempCompensationRaw - 25){
           if(absorptionStartTime == 0) absorptionStartTime = currentTime;
@@ -199,17 +200,19 @@ void Charging_Algorithm(float sol_volts, unsigned long currentTime) {
           off_count = OFF_NUM;                                // go to the charger off state  
           set_pwm_duty(true);         
         }        
-        else if (rawBatteryV > floatVoltageRaw + tempCompensationRaw - powerCompensation) {                    // If we've charged the battery above the float voltage                   
+        else if (rawBatteryV > effectiveBound) {                    // If we've charged the battery above the float voltage                   
           int delta = (rawBatteryV - (floatVoltageRaw + tempCompensationRaw - powerCompensation)) / 2;
           // Serial.print(batteryV);Serial.print("decrease "); Serial.println(delta);
           duty -= delta;                                      // down
           set_pwm_duty(false);                                     // write the PWM
         }
-        else if (rawBatteryV < floatVoltageRaw + tempCompensationRaw - powerCompensation) {                    // else if the battery voltage is less than the float voltage - 0.1
+        else if (rawBatteryV <= effectiveBound) {                    // else if the battery voltage is less than the float voltage - 0.1
           //int delta = 2; // (batteryV - (floatVoltage + tempCompensation - powerCompensation)) / 0.01;
           // Serial.print(batteryV);Serial.print("increase "); Serial.println(delta);
-          //duty += delta;                                              // up
-          //set_pwm_duty(false); // protect duty from drifting up                                     
+          if(rawBatteryV < effectiveBound - 10){
+            duty += 2;                                              // up
+            set_pwm_duty(false); // protect duty from drifting up                                     
+          }
           if (rawBatteryV < floatVoltageRaw + tempCompensationRaw - 40){ //(floatVoltage + tempCompensation - 0.6)) {               // if the voltage drops because of added load,
             absorptionAccTime += currentTime - absorptionStartTime;
             absorptionStartTime = 0;
