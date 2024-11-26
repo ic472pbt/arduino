e4c1c7f8f040b1ddc9e6fc0bb8dad283c0261485
Pavel Tatarintsev <ic472pbt@ya.ru>
2024-11-26 12:35:44 +0400

    try gess best duty, avoid scan

--- MPPT/scanState.cpp
+++ MPPT/scanState.cpp
@@ -4,21 +4,24 @@
 IState* scanState::Handle(Charger& charger, SensorsData& sensor, unsigned long currentTime) 
  {
         IState* newState = this;        
-        if(cycleNum == 10){
+        if(cycleNum == 1){
           cycleNum = 0;
-          bestPower = 0;
+          // bestPower = 0;
+          bestDuty = (unsigned int)(sensor.batteryV * 1023.0 / sensor.PVvoltage); // try guess best duty
           charger.pwmController.setDuty(bestDuty);
           charger.pwmController.initIIR();
           newState = charger.goBulk(currentTime);
           charger.startTracking = true;
         }else{
           cycleNum++;
-          unsigned long solarPower = (unsigned long)charger.rawCurrentIn * sensor.rawBatteryV;
+          charger.pwmController.shutdown();
+          /* old algorithm
+           *  unsigned long solarPower = (unsigned long)charger.rawCurrentIn * sensor.rawBatteryV;
           if(solarPower > bestPower){
             bestPower = solarPower;
             bestDuty = charger.pwmController.duty;
           }
-          charger.pwmController.incrementDuty(SCAN_STEP);    
+          charger.pwmController.incrementDuty(SCAN_STEP);    */
         }
         return newState;   
     };
