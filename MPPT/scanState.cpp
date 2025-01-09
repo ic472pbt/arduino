d753fd964f71e6e8c2ef84c09f47a2aabdecfb37
Pavel Tatarintsev <ic472pbt@ya.ru>
2025-01-09 09:15:13 +0400

    mcu original wiring

--- MPPT/scanState.cpp
+++ MPPT/scanState.cpp
@@ -6,7 +6,7 @@ IState* scanState::Handle(Charger& charger, SensorsData& sensor, unsigned long c
         IState* newState = this;        
         if(cycleNum == 1){
           cycleNum = 0;
-          bestDuty = (unsigned int)(sensor.batteryV * 1023.0 / sensor.PVvoltage) + 70; // try to guess best duty
+          bestDuty = (unsigned int)(sensor.batteryV * 1023.0 / sensor.PVvoltage) + 70; // try to guess best duty offset (pt) = 0.175 (pt) * battery capacity (Ah)
           charger.pwmController.setDuty(bestDuty);
           charger.pwmController.initIIR();
           newState = charger.goBulk(currentTime);
